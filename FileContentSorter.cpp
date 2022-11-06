5b7d9e27cb83f318ce1e0751f3544e6efeade126
Isaiah-Martinez <82429414+Isaiah-Martinez@users.noreply.github.com>
2022-11-06 01:55:47 -0800

    Updated to work with vectors instead of lists. Current task is to take the output sorted results and output them to the file

--- FileContentSorter.cpp
+++ FileContentSorter.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
-#include <fstream>
+#include <fstream> //iftream
+#include <sstream> //istream and istringstream
+#include <algorithm> //sort and copy
 #include <string>
-#include <list>
-#include <sstream>
-#include <utility>
+#include <vector>
+#include <tuple>
 
 using namespace std;
 
-list<string> backwardsSorter(list<string> listString);
+vector<string> backwardsSorter(vector<string> vectorString);
 
 int main()
 {
     //opening and reading from the file
     //note that the file needs to have an empty line at the end of it for proper reading
     ifstream myfile;
-    myfile.open("shopping_list.txt");                                                   //enter file name for the file to be sorted here
+    myfile.open("sample_file.txt");
 
     //makes a list that will hold the data to be sorted
-    list<string> fileContents = {};
+    vector<string> fileContents;
     
     //if file is open does operation O/W prints msg to user
     if (myfile.is_open()) {
@@ -38,7 +39,7 @@ int main()
 
         do {
             try {
-                cout << "Would you like to sort by through the First method or the Second method?\nRespond '0' for First and '1' for Second\n";
+                cout << "Would you like to sort by First Word or the Last Word?\nRespond '0' for First and '1' for Second\n";
                 cin >> userInput;
 
                 //checks userInput for validity. If invalid will throw userInput. O/W will sort based on user choice
@@ -46,22 +47,29 @@ int main()
                     throw userInput;
                 }
                 else {
-                    cout << "Response is: " << userInput << endl;
+                    cout << "Response is: " << userInput << endl << endl;
                     hasRun = !hasRun;
+
                     if ((userInput.compare("0") == 0)) {
-                        fileContents.sort();
+                        sort(fileContents.begin(), fileContents.end());
+
+                        cout << "Sorted by First word" << endl << endl;
                     }
                     else {
-                        //delete the following line once backwardsSorter works
-                        //cout << "sort backwards" << endl;
-                        backwardsSorter(fileContents);
+                        fileContents = backwardsSorter(fileContents);
+
+                        cout << "Sorted by Last word" << endl << endl;
                     }
-                    /*
+                    
                     //print contents of the array
                     for (string str : fileContents) {
                         cout << str << '\n';
                     }
-                    */
+
+                    //now want to output fileContents into the file that was read
+
+
+
                 }
             }
             catch (...) {
@@ -74,38 +82,54 @@ int main()
     }
 }
 
-list<string> backwardsSorter(list<string> listString) {
-    //i for index and a list that will be sorted
+//will sort based on the last "word" of the line in vectorString
+vector<string> backwardsSorter(vector<string> vectorString) {
+    //i for iterating
+    //vector will hold a list of tuples that
+    //contain: the last "word", index number, and size of "line"
     int i = 0;
-    list<pair <string, int>> toSort;
+    vector<tuple <string, int, int>> lastWords;
 
-    //this grabs all info and puts into a list of pairs
-    for (string str : listString) {
-        list<string> last;
+    //this grabs all info and puts into a vector of tuples
+    for (string str : vectorString) {
+        vector<string> last;
         istringstream iss(str);
         copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter(last));
         if (!(last.empty())) {
-            pair<string, int> toSortPair(last.back(), i);
-            toSort.push_back(toSortPair);
+            lastWords.push_back(make_tuple(last.back(), i, str.size()));
+            //last.size() gives # words in string
+            //last[#].size() gives length of last word
         }
         i++;
     }
-
-    //sort the list by the strings
-    toSort.sort();
-
-    //now we will match the index within the pairs to their respective strings to return
-
-
-
-    /*
-    //print list
-    for (pair<string, int> toSortPair : toSort) {
-        cout << toSortPair.first << " " << toSortPair.second;
+    
+    sort(lastWords.begin(), lastWords.end());
+    //now the words are in alphabetical order
+    //however there may be lines that are longer than others that are out of desired order
+    
+    //notice that i now holds the length of lastWords
+    //We will now use j as our iterator
+    int j = 0;
+    
+    //will sort by length of string ( get<2>lastWords[INDEX] ) for each character 
+    while (j != i - 2) {
+        if ((get<0>(lastWords[j]).front() == get<0>(lastWords[j + 1]).front()) && (get<2>(lastWords[j]) > get<2>(lastWords[j + 1]))) {
+            //may want to add additional conditions based on other
+            //words coming before the final one
+            tuple <string, int, int> temp = lastWords[j];
+            lastWords[j] = lastWords[j + 1];
+            lastWords[j + 1] = temp;
+            j = -1;
+        }
+        j++;
     }
-    */
 
+    //now want to insert the now sorted array from lastWords into a sortedVector
+    vector<string> sortedVector;
 
+    for (j = 0; j < i - 1; j++) {
+        sortedVector.push_back(vectorString[get<1>(lastWords[j])]);
+    }
 
-    return listString;
+    return sortedVector;
 }
